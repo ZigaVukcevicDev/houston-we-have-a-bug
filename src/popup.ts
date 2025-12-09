f9fef15989c395ccae833ed7d712aa85ab0e44c8
Ziga Vukcevic <ziga.vukcevic@gmail.com>
2025-12-09 19:56:38 +0100

    Refactor ids to classes

--- src/popup.ts
+++ src/popup.ts
@@ -1,25 +1,35 @@
 import { getChromeVersion } from './utils/get-chrome-version';
+import { getDateAndTime } from './utils/get-date-and-time';
 import { getOS } from './utils/get-os';
 import { getTabViewport } from './utils/get-tab-viewport';
 
-document.getElementById('actionButton')?.addEventListener('click', async () => {
-  const urlElement = document.getElementById('url')!;
-  const browserElement = document.getElementById('browser')!;
-  const osElement = document.getElementById('os')!;
-  const viewportElement = document.getElementById('viewport')!;
+document
+  .querySelector('[data-js="action-button"]')
+  ?.addEventListener('click', async () => {
+    const elements = {
+      dateAndTime: document.querySelector('[data-js="date-and-time"]')!,
+      url: document.querySelector('[data-js="url"]')!,
+      browser: document.querySelector('[data-js="browser"]')!,
+      os: document.querySelector('[data-js="os"]')!,
+      viewport: document.querySelector('[data-js="viewport"]')!,
+    };
 
-  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    const [tab] = await chrome.tabs.query({
+      active: true,
+      currentWindow: true,
+    });
 
-  if (tab?.url && tab.id) {
-    const viewport = await getTabViewport(tab.id);
-    urlElement.textContent = `URL: ${tab.url}`;
-    viewportElement.textContent = `Viewport: ${viewport}`;
-    browserElement.textContent = `Browser: ${getChromeVersion(navigator.userAgent)}`;
-    osElement.textContent = `OS: ${getOS(navigator.userAgent)}`;
-  } else {
-    urlElement.textContent = 'Could not get URL';
-    browserElement.textContent = '';
-    osElement.textContent = '';
-    viewportElement.textContent = '';
-  }
-});
+    if (tab?.url && tab.id) {
+      elements.dateAndTime.textContent = `Date and time: ${getDateAndTime()}`;
+      elements.url.textContent = `URL: ${tab.url}`;
+      elements.viewport.textContent = `Viewport: ${await getTabViewport(tab.id)}`;
+      elements.browser.textContent = `Browser: ${getChromeVersion(navigator.userAgent)}`;
+      elements.os.textContent = `OS: ${getOS(navigator.userAgent)}`;
+    } else {
+      elements.dateAndTime.textContent = '';
+      elements.url.textContent = 'Could not get URL';
+      elements.browser.textContent = '';
+      elements.os.textContent = '';
+      elements.viewport.textContent = '';
+    }
+  });
