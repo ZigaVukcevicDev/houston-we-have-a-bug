a06173acafb623a4e27476fa2810df7273d6745d
Ziga Vukcevic <ziga.vukcevic@gmail.com>
2025-12-15 19:31:53 +0100

    Move command to script file

---
+++ scripts/copy-files.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+rm -rf dist
+mkdir -p dist
+cp src/entrypoints/popup.html src/entrypoints/tab.html manifest.json dist/
+cp -r src/images dist/
+cp -r src/fonts dist/
+mkdir -p dist/styles
+cp src/styles/fonts.css dist/styles/fonts.css
