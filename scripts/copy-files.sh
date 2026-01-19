6f57f95679aef68a90aa404f70f038a42616ecab
Ziga Vukcevic <ziga.vukcevic@gmail.com>
2026-01-19 17:21:45 +0100

    fix: add .chromeignore to copy-files script for improved file management

--- scripts/copy-files.sh
+++ scripts/copy-files.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-cp src/entrypoints/popup.html src/entrypoints/tab.html manifest.json test-page.html dist/
+cp src/entrypoints/popup.html src/entrypoints/tab.html manifest.json test-page.html .chromeignore dist/
 rsync -a --exclude='extension-presentation' src/images/ dist/images/
 cp -r src/fonts dist/
 mkdir -p dist/styles
